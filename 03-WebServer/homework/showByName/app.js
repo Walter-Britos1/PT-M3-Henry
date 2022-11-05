e166707c13912060c8ea975670a05f7051e9584e
wal7erB <103972809+wal7erB@users.noreply.github.com>
2022-11-05 20:28:21 +0000

    homework 03-WebServer terminado

--- 03-WebServer/homework/showByName/app.js
+++ 03-WebServer/homework/showByName/app.js
@@ -2,3 +2,14 @@ var fs  = require("fs")
 var http  = require("http")
 
 // Escribí acá tu servidor
+http.createServer ((req, res) => {
+    fs.readFile(`./images${req.url}.jpg`, (err, data) => {
+        if (err){
+            res.writeHead(400, {'Content-Type' : 'text/plain'});
+            res.end('Img not found...')
+        }else {
+            res.writeHead(200, {'Content-Type' : 'image/jpeg'});
+            res.end(data)
+        }
+    })
+}).listen(1337, '127.0.0.1')
